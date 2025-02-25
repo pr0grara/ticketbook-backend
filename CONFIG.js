1bb7671b8eb6ba5982bb74df11ef164f18a579fc
Ara Z Baghdassarian <azbaghda@gmail.com>
2025-02-25 09:02:01 -0800

    remove "/" from cors origins

--- CONFIG.js
+++ CONFIG.js
@@ -1,8 +1,8 @@
 const CORS_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:8080",
-    "http://ticketbook-frontend-env.us-west-1.elasticbeanstalk.com/",
-    "http://test-env.eba-ebsjy3my.us-west-1.elasticbeanstalk.com/"
+    "http://ticketbook-frontend-env.us-west-1.elasticbeanstalk.com",
+    "http://test-env.eba-ebsjy3my.us-west-1.elasticbeanstalk.com"
 ];
 
 module.exports = ({CORS_ORIGINS});
